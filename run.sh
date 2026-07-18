6da0a7b013750db9179d96efe98860ea58dcf807
Queen Vinyl Da.i'gyu-Kazotetsu <queengooborg@gmail.com>
2026-07-17 23:06:16 -0700

    Disable incremental build on testing server
    This seems to be more problematic than helpful

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 # Run the local test web server
 
-bundle exec jekyll serve --host 0.0.0.0 --livereload --incremental --ssl-cert localhost.pem --ssl-key localhost-key.pem
+bundle exec jekyll serve --host 0.0.0.0 --livereload --ssl-cert localhost.pem --ssl-key localhost-key.pem
